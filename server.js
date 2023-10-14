22ac95fd21db23086a35b8ea95b231dee90aabb7
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-10-13 21:22:03 -0400

    BRR fix: mongoose connection and member schema building

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const dotenv = require('dotenv');
 dotenv.config()
 const http = require('http');
-const mongodb = require('mongodb');
+const mongoose = require('mongoose');
 
 const connectionString = process.env.MONGO_URL
 
-mongodb.connect(
+mongoose.connect(
     connectionString,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     },
-    (err, client) => {
+    (err, goose) => {
         if (err) console.log("Error connecting to MongoDB");
         else {
             console.log("Connecting to MongoDB successfully")
+            console.log(goose)
             const app = require('./app.js')
             const server = http.createServer(app)
             let PORT = process.env.PORT || 3003
             server.listen(PORT, () => {
-                console.log(`The server is listening successfully on ${PORT}`)
+                console.log(`The server is listening successfully on ${PORT}, http://localhost:${PORT}`)
             })
         }
     })
