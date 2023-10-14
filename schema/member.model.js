8a991ab6aba87189e750c1db2c267007891240fd
rakhimboev <dilshodbekrakhimbaev@gmail.com>
2023-10-13 22:52:45 -0400

    BRR fix: modifying member schema model part 2

--- schema/member.model.js
+++ schema/member.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { member_type_enums, member_status_enums } = require('../lib/config')
+const { member_type_enums, member_status_enums, ordinary_enums } = require('../lib/config')
 
 const memberSchema = new mongoose.Schema({
     mb_nick: {
@@ -34,11 +34,11 @@ const memberSchema = new mongoose.Schema({
             message: "{VALUE} is not among permitted values"
         }
     },
-    mb_full_name: {
+    mb_address: {
         type: String,
         required: false,
     },
-    mb_address: {
+    mb_description: {
         type: String,
         required: false,
     },
@@ -51,7 +51,38 @@ const memberSchema = new mongoose.Schema({
         required: false,
         default: 0
     },
+    mb_top: {
+        type: String,
+        required: false,
+        default: 'N',
+        enum: {
+            values: ordinary_enums,
+            message: "{VALUE} is not among permitted values"
 
-})
+        }
+    },
+    mb_views: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    mb_likes: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    mb_follow_cnt: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+    mb_subscriber_cnt: {
+        type: Number,
+        required: false,
+        default: 0,
+    },
+},
+{ timestamps: true },
+)
 
 module.exports = mongoose.model('Member', memberSchema);
